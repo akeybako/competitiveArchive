11c236121d077049382b502dfc4546e99765d9a7
akeybako <lespe1014@gmail.com>
2020-10-15 23:22:31 +0900

    add IO template

---
+++ templates/Kotlin/IO.kt
@@ -0,0 +1,8 @@
+fun next() = readLine()!!
+fun nextInt() = next().toInt()
+fun nextLong() = next().toLong()
+fun nextDouble() = next().toDouble()
+fun nextList() = next().split(" ")
+fun nextIntList() = next().split(" ").map{ it.toInt() }
+fun nextLongList() = next().split(" ").map{ it.toLong() }
+fun nextDoubleList() = next().split(" ").map{ it.toDouble() }
